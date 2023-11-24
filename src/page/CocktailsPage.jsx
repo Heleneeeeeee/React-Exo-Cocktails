13eb61de5b3c4710ae5109aa0f7265cd9d3bc3ce
Helene <helene.quintard@lapiscine.pro>
2023-11-24 11:14:15 +0100

    Add Await Fetch

--- src/page/CocktailsPage.jsx
+++ src/page/CocktailsPage.jsx
@@ -6,15 +6,26 @@ const CocktailsPage = () => {
     const [cocktails, setCocktails] = useState (null)
 
     if (!cocktails){
-        fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
-            .then((response)=>{
-            return (
-            response.json())
-            })
-            .then ((cocktailsInjson)=>{
-                setCocktails(cocktailsInjson.drinks) 
-             });
-    }
+    //     fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+    //         .then((cocktailsResponse)=>{
+    //         return (
+    //         cocktailsResponse.json())
+    //         })
+    //         .then ((cocktailsInjson)=>{
+    //             setCocktails(cocktailsInjson.drinks) 
+    //          });
+    // }
+
+            (async() =>{
+                const cocktailsResponse = await fetch ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+                //Je déclare une variable qui a pour fonction de récupérer l'API en utilisant fetch. Puisqu'elle est asynchrone je dois lui passer
+                //comme mot-clé await
+                const cocktailsInjson = await cocktailsResponse.json()
+                //Je déclare une nouvelle variable pour récuper le fichier JSON. Je dois également lui faire faire passer le mot-clé await
+                setCocktails(cocktailsInjson.drinks)
+                // Je stocke ensuite le fichier dans setCcoktails
+            })();
+        }
 
     return (
         <main>
@@ -39,3 +50,5 @@ const CocktailsPage = () => {
 }
  
 export default CocktailsPage;
+
+
